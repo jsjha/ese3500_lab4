cda07e7672b04876713223c4822ed9b2573d8756
Jessi Jha <jessijha@seas.upenn.edu>
2024-03-27 15:07:30 -0400

    Set up (again idk why)

---
+++ Pong/Pong/main.c
@@ -0,0 +1,18 @@
+/*
+ * Pong.c
+ *
+ * Created: 3/27/2024 3:03:16 PM
+ * Author : jessi
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
